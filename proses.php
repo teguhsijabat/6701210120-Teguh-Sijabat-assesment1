3e914cecb31f68c2ea228670fa54e61d65f5214f
teguhsijabat <teguhsijabat55@gmail.com>
2023-03-27 09:57:35 +0700

    v1.2.1

---
+++ proses.php
@@ -0,0 +1,28 @@
+<?php
+
+if(isset($_POST['submit'])){
+    $angka1 = $_POST['angka1'];
+    $angka2 = $_POST['angka2'];
+    $operator = $_POST['operator'];
+
+    switch ($operator) {
+        case '+':
+            $hasil = $angka1 + $angka2;
+            break;
+        case '-':
+            $hasil = $angka1 - $angka2;
+            break;
+        case '*':
+            $hasil = $angka1 * $angka2;
+            break;
+        case '/':
+            $hasil = $angka1 / $angka2;
+            break;
+        default:
+            $hasil = "Tidak dapat melakukan operasi aritmatika";
+            break;
+    }
+
+    echo "Hasil operasi aritmatika dari " . $angka1 . " " . $operator . " " . $angka2 . " adalah " . $hasil;
+}
+?>
